4312e09812a007ad3b2e4731bf1caa5f4a0970dc
RonnyMite <Ronnymite15@gmail.com>
2026-08-28 11:01:00 -0500

    Create Cliente class with getters and setters

---
+++ cliente.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class Cliente(ABC):
+
+    def __init__(self, cedula, nombre, correo):
+        self.__cedula = cedula
+        self.__nombre = nombre
+        self.__correo = correo
+
+    def get_cedula(self):
+        return self.__cedula
+
+    def set_cedula(self, cedula):
+        self.__cedula = cedula
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def set_nombre(self, nombre):
+        self.__nombre = nombre
+
+    def get_correo(self):
+        return self.__correo
+
+    def set_correo(self, correo):
+        self.__correo = correo
+
+    @abstractmethod
+    def calcular_descuento(self, monto):
+        pass
